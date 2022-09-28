2a9f882f01a931f4e2f645688263f1988ba7d5a1
fe-i <feic47@gmail.com>
2022-09-28 12:29:55 -0400

    reorganize code

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -4,25 +4,24 @@ public class TipCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in); // create a scanner
 
-        System.out.print("Total bill: $");
+        System.out.print("Input the total bill: $");
         double totalBill = scanner.nextDouble(); // collect user input for the total bill
 
-        System.out.print("Tip percentage (without % symbol): ");
-        int tipPercentage = scanner.nextInt(); // collect user input for the tip percentage
+        System.out.print("Input the tip percentage as a whole number (without % symbol): ");
+        double tipPercentage = scanner.nextDouble(); // collect user input for the tip percentage
 
         System.out.print("Number of people: ");
         int numberOfPeople = scanner.nextInt(); // collect user input for the number of people
 
         System.out.println("----------------------------");
 
-        double totalTip = totalBill * (tipPercentage / 100.0); // calculate the tip
-        System.out.println("The total tip amount is $" + String.format("%.2f", totalTip)); // print the tip
-
+        double totalTip = totalBill * (tipPercentage / 100); // calculate the tip
         totalBill += totalTip; // add the tip to the total bill
-        System.out.println("The total bill is $" + String.format("%.2f", totalBill)); // print the update total bill
-
         double totalPerPerson = totalBill / numberOfPeople; // calculate how much each person pays
-        System.out.println("The total cost per person is $" + String.format("%.2f", totalPerPerson)); // output the cost per person
+
+        System.out.println("After a tip of $" + String.format("%.2f", totalTip) + // print the tip
+                ", the total bill is $" + String.format("%.2f", totalBill) + // print the update total bill
+                "\nDivided among the " + numberOfPeople + " people, each person will pay $" + String.format("%.2f", totalPerPerson)); // print the cost per person
 
         scanner.close();
     }
